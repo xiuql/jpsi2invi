e1d4c02ed7070c383846cb9ee8b7e99542fb20ad
SHI Xin <shixin@ihep.ac.cn>
2016-05-13 07:48:40 +0800

    clean up non-used code

--- Analysis/Physics/PsiPrime/Jpsi2invi/Jpsi2invi-00-00-01/src/Jpsi2invi.cc
+++ Analysis/Physics/PsiPrime/Jpsi2invi/Jpsi2invi-00-00-01/src/Jpsi2invi.cc
@@ -118,23 +118,16 @@ private:
   int selectPionPlusPionMinus(SmartDataPtr<EvtRecTrackCol>,
 			      std::vector<int>,
 			      std::vector<int>);
-
   void calcTrackPID(EvtRecTrackIterator,
 		    double& ,
 		    double&);
   bool hasGoodPiPiVertex(RecMdcKalTrack *,
 			 RecMdcKalTrack *);
-  
   int selectNeutralTracks(SmartDataPtr<EvtRecEvent>,
 			  SmartDataPtr<EvtRecTrackCol>);
-  
-  bool passPreSelection();
   bool passVertexSelection(CLHEP::Hep3Vector,
 			   RecMdcKalTrack* ); 
   CLHEP::Hep3Vector getOrigin();
-  bool passPhotonSelection(SmartDataPtr<EvtRecEvent>,
-			   SmartDataPtr<EvtRecTrackCol>); 
-  
 }; 
 
 
@@ -153,27 +146,15 @@ LOAD_FACTORY_ENTRIES( Jpsi2invi )
 // constants
 //
 
-//mass: e,mu,pi,k,p,ks,p,pi0,etac,jpsi
-
-// const double ELECTRON_MASS = 0.000511;
-// const double MUON_MASS = 0.105658;
 const double PION_MASS = 0.139570;
-// const double KAON_MASS = 0.493677;
-// const double KSHORT_MASS = 0.497614;
-// const double PROTON_MASS = 0.938272;
-// const double PIONZERO_MASS = 0.134977;
-// const double ETAC_MASS = 2.98;
-// const double JPSI_MASS = 3.0969;
-// const double VELC = 299.792458;   // tof path unit in mm
 
-  
+
 //
 // member functions
 //
   
 Jpsi2invi::Jpsi2invi(const std::string& name, ISvcLocator* pSvcLocator) :
   Algorithm(name, pSvcLocator) {
-  //Declare the properties
   declareProperty("Ecms",m_ecms = 3.686);
   declareProperty("Vr0cut", m_vr0cut=1.0);
   declareProperty("Vz0cut", m_vz0cut=10.0);
@@ -189,7 +170,6 @@ Jpsi2invi::Jpsi2invi(const std::string& name, ISvcLocator* pSvcLocator) :
   declareProperty("EnergyBarrelMin", m_energy_barrel_min=0.025); 
   declareProperty("EnergyEndcapMin", m_energy_endcap_min=0.050); 
   declareProperty("PhotonIsoAngleMin", m_photon_iso_angle_min=10);
-
   declareProperty("PionPolarAngleMax", m_pion_polar_angle_max=0.8);
   declareProperty("PionMomentumMax", m_pion_momentum_max=0.45); 
   declareProperty("ProbPionMin", m_prob_pion_min=0.001);
@@ -197,7 +177,6 @@ Jpsi2invi::Jpsi2invi(const std::string& name, ISvcLocator* pSvcLocator) :
   declareProperty("DipionMassMax", m_dipion_mass_max=3.2); 
   declareProperty("PiPiCosthetaMax", m_pipi_costheta_max=0.95);
   declareProperty("PiPiSysCosthetaMax", m_pipisys_costheta_max=0.90);
-  
 }
 
 
@@ -207,12 +186,10 @@ StatusCode Jpsi2invi::initialize(){
 
   if (! book_ntuple_signal(log) ) return StatusCode::FAILURE;
 
-
   //--------end booking --------
 
   log << MSG::INFO << "successfully return from initialize()" <<endmsg;
   return StatusCode::SUCCESS;
-
 }
 
 
@@ -226,8 +203,6 @@ StatusCode Jpsi2invi::execute() {
   m_run = eventHeader->runNumber();
   m_event = eventHeader->eventNumber();
 
-
-  //if (!passPreSelection()) return StatusCode::FAILURE;
   buildJpsiToInvisible();
   
   m_tuple1->write();
@@ -273,7 +248,6 @@ bool Jpsi2invi::book_ntuple_signal(MsgStream log) {
       // neutral tracks
       status = m_tuple1->addItem ("nshow",  m_nshow);
       status = m_tuple1->addItem ("ngam",  m_ngam);
-
       
     } else { 
       log << MSG::ERROR << "    Cannot book N-tuple:" 
@@ -299,67 +273,8 @@ void Jpsi2invi::buildJpsiToInvisible() {
   selectPionPlusPionMinus(evtRecTrkCol, iPGood, iMGood); 
   
   selectNeutralTracks(evtRecEvent, evtRecTrkCol);
-
 }
 
-
-bool Jpsi2invi::passPreSelection() {
-
-  MsgStream log(msgSvc(), name());
-  SmartDataPtr<Event::EventHeader>eventHeader(eventSvc(),"/Event/EventHeader");
-  if(!eventHeader){
-    log << MSG::ERROR << "EventHeader not found" << endreq;
-    return false;
-  }
-  m_run = eventHeader->runNumber();
-  m_event = eventHeader->eventNumber();
-
-  SmartDataPtr<EvtRecEvent>evtRecEvent(eventSvc(),"/Event/EvtRec/EvtRecEvent");
-  if(!evtRecEvent) return false; 
-
-  SmartDataPtr<EvtRecTrackCol> evtRecTrkCol(eventSvc(), "/Event/EvtRec/EvtRecTrackCol");
-  if(!evtRecTrkCol) return false;
-
-  CLHEP::Hep3Vector xorigin = getOrigin(); 
-
-  // Good Kalman Track
-  std::vector<int> iGood;
-  iGood.clear();
-  int nCharge = 0;
-  m_pion_matched = 0;
-  m_lep_matched = 0;
-  
-  // loop through charged tracks 
-  for(int i = 0; i < evtRecEvent->totalCharged(); i++){
-    // get mdcTrk 
-    EvtRecTrackIterator itTrk=evtRecTrkCol->begin() + i;
-
-    // Good Kalman Track 
-    if(! ((*itTrk)->isMdcKalTrackValid()) ) continue;
-    
-    RecMdcKalTrack* mdcTrk = (*itTrk)->mdcKalTrack();
-    if(mdcTrk->p()<m_distin_pionlep)
-      mdcTrk->setPidType(RecMdcKalTrack::pion);
-    else
-      mdcTrk->setPidType(RecMdcKalTrack::muon);
-
-    // Good Vertex 
-    if (!passVertexSelection(xorigin, mdcTrk) ) continue; 
-
-    // Polar angle cut
-    if(fabs(cos(mdcTrk->theta())) > m_cha_costheta_cut) continue;
-
-    iGood.push_back(i);
-    nCharge += mdcTrk->charge();
-    
-  } // end charged tracks
-  
-  // Photon selection
-  if (!passPhotonSelection(evtRecEvent, evtRecTrkCol) )
-    return false;
-
-  return true; 
-}
 
 CLHEP::Hep3Vector Jpsi2invi::getOrigin() {
   CLHEP::Hep3Vector xorigin(0,0,0);
@@ -393,74 +308,6 @@ bool Jpsi2invi::passVertexSelection(CLHEP::Hep3Vector xorigin,
   
   return true;
   
-}
-
-
-bool Jpsi2invi::passPhotonSelection(SmartDataPtr<EvtRecEvent> evtRecEvent,
-				    SmartDataPtr<EvtRecTrackCol> evtRecTrkCol){
-  
-  std::vector<int> iGam; 
-  // loop through neutral tracks
-  for(int i=evtRecEvent->totalCharged(); i< evtRecEvent->totalTracks(); i++) {
-    if (i > m_total_number_of_charged_max) break;
-
-    EvtRecTrackIterator itTrk = evtRecTrkCol->begin() + i ;
-    if(!(*itTrk)->isEmcShowerValid()) continue;
-    RecEmcShower *emcTrk = (*itTrk)->emcShower();
-
-    // TDC window
-    if ( !(emcTrk->time() >= m_min_emctime && emcTrk->time() <= m_max_emctime) )
-      continue; 
-
-    // Energy threshold
-    double abs_costheta(fabs(cos(emcTrk->theta())));
-    bool barrel = (abs_costheta < m_costheta_barrel_max); 
-    bool endcap = (abs_costheta > m_costheta_endcap_min
-		   && abs_costheta < m_costheta_barrel_max);
-    double eraw = emcTrk->energy();
-    
-    if ( !( (barrel && eraw > m_energy_barrel_min)
-	    || (endcap && eraw > m_energy_endcap_min)))  continue; 
-
-    // photon isolation: the opening angle between a candidate shower
-    // and the closest charged track should be larger than 10 degree 
-    CLHEP::Hep3Vector emcpos(emcTrk->x(), emcTrk->y(), emcTrk->z());
-
-    // EMC costheta cut 
-    double costhe = cos(emcpos.theta());
-    if ( fabs(costhe) >= m_gammaCosCut) continue;
-
-    // find the nearest charged track
-    double dthe = 200.;
-    double dphi = 200.;
-    double dang = 200.; 
-    for(int j = 0; j < evtRecEvent->totalCharged(); j++) {
-      EvtRecTrackIterator jtTrk = evtRecTrkCol->begin() + j;
-      if(!(*jtTrk)->isExtTrackValid()) continue;
-      RecExtTrack *extTrk = (*jtTrk)->extTrack();
-      if(extTrk->emcVolumeNumber() == -1) continue;
-      CLHEP::Hep3Vector extpos = extTrk->emcPosition();
-      double angd = extpos.angle(emcpos);
-      double thed = extpos.theta() - emcpos.theta();
-      double phid = extpos.deltaPhi(emcpos);
-      thed = fmod(thed+CLHEP::twopi+CLHEP::twopi+pi, CLHEP::twopi) - CLHEP::pi;
-      phid = fmod(phid+CLHEP::twopi+CLHEP::twopi+pi, CLHEP::twopi) - CLHEP::pi;
-
-      if(fabs(thed) < fabs(dthe)) dthe = thed;
-      if(fabs(phid) < fabs(dphi)) dphi = phid;
-      if(angd < dang) dang = angd;	    
-    }
-
-    if(dang>=200) continue;
-    dthe = dthe * 180 / (CLHEP::pi);
-    dphi = dphi * 180 / (CLHEP::pi);
-    dang = dang * 180 / (CLHEP::pi);
-    if (dang < m_photon_iso_angle_min ) continue; 
-
-    iGam.push_back(i); 
-  } // end loop neutral tracks 
-    
-  return true; 
 }
 
 
