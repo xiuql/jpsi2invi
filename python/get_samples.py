08764588a5cc6b8b085b8d7d32604ca782731ea1
SHI Xin <shixin@ihep.ac.cn>
2016-05-31 09:49:17 +0800

    add file size option

--- python/get_samples.py
+++ python/get_samples.py
@@ -18,9 +18,12 @@ NAME
 
 SYNOPSIS
 
-    ./get_samples.py  input_dir output_file
+    ./get_samples.py  input_dir output_file [size]
 
     ./get_samples.py  /bes3fs/offline/data/664p03/psip/dst ../samples/data_664p03_psip.txt
+
+    ./get_samples.py  /bes3fs/offline/data/664p03/psip/dst ../samples/data_664p03_psip.txt 40G 
+
 
 AUTHOR 
     SHI Xin <shixin@ihep.ac.cn> 
@@ -32,20 +35,39 @@ DATE
     
 def main():
     args = sys.argv[1:]
-    if len(args) != 2:
+    if len(args) < 2:
         return usage()
     
     src = args[0]
     dst = args[1]
-
     sys.stdout.write('Scanning %s...\n' %src)
+
     file_list = []
     for root, dirs, files in os.walk(src):
         for f in files:
             file_list.append(os.path.join(root, f))
 
-    nfiles = len(file_list)
+    if len(args) == 2:
+        save_list_into_file(file_list, dst)
+        return
+    
+    if len(args) == 3:
+        size = args[2]
+        sys.stdout.write('Size limit per list: %s\n' %size)
+        groups = group_files_by_size(file_list, size)
+        path, name = os.path.split(dst)
+        dst_com = name.split('.')[0] 
+        dst_ext = name.split('.')[1]
+        n = 0 
+        for file_list in groups:
+            n = n+1 
+            new_dst = os.path.join(path, '%s_%s-%s.%s' %
+                                   (dst_com, size, n, dst_ext))
+            save_list_into_file(file_list, new_dst)
+        
 
+def save_list_into_file(file_list, dst):
+    nfiles = len(file_list)
     fo = open(dst, 'w')
     fo.write('EventCnvSvc.digiRootInputFile = {\n')
 
@@ -61,5 +83,35 @@ def main():
     sys.stdout.write('Saved as: %s\n' %dst)
 
     
+def group_files_by_size(name_list, size_max='40G'):
+    size_max = convert_size_from_str(size_max)
+    groups = []
+    group = []    
+    size_sum = 0
+
+    for name in name_list:
+        size = os.path.getsize(name)
+        if size_sum < size_max:
+            group.append(name)
+            size_sum += float(size)
+        else:
+            groups.append(group)
+            group = []
+            size_sum = 0
+            group.append(name)            
+            size_sum += float(size)
+
+        if name == name_list[-1]:
+            groups.append(group)
+
+    return groups
+
+
+def convert_size_from_str(size_str):
+    c_1GB = 1024*1024*1024
+    factor = eval(size_str.split('G')[0])
+    return factor*c_1GB
+
+
 if __name__ == '__main__':
     main()
